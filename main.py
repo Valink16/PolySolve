d7a2536c8a349b84b9dc696e13394d75497331ba
Valink16 <valuthyk@gmail.com>
2020-09-21 10:30:46 +0200

    Implemented roots computing

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ b = Poly.from_str("x + 1")
 c = Poly.from_str("x + 4")
 
 f = a * b * c
+g = b * c
 
 print(f)
-
-print(f // Poly.from_str("x - 5"))
-print(f % Poly.from_str("x - 5"))
-
-
+print(f.roots())
--- poly.py
+++ poly.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
 from re import sub, split, finditer, search
 
 class Poly:
 	def __init__(self, coefficients: dict):
 		self.coefficients = coefficients
+
+		self.refresh()
 
 	def __repr__(self):
 		s = ""
@@ -20,7 +23,7 @@ class Poly:
 
 		return s if len(s) > 0 else '0'
 
-	def __sub__(self, other):
+	def __sub__(self, other: Poly):
 		current = dict(self.coefficients)
 		for k in other.coefficients:
 			if k in current:
@@ -31,7 +34,18 @@ class Poly:
 				current[k] = -other.coefficients[k]
 		return Poly(current)
 
-	def __floordiv__(self, other):
+	def __add__(self, other: Poly):
+		current = dict(self.coefficients)
+		for k in other.coefficients:
+			if k in current:
+				current[k] += other.coefficients[k]
+				if current[k] == 0:
+					current.pop(k)
+			else:
+				current[k] = other.coefficients[k]
+		return Poly(current)
+
+	def __floordiv__(self, other: Poly):
 		other_degrees = list(other.coefficients.keys())
 		other_degrees.sort()
 
@@ -62,7 +76,7 @@ class Poly:
 			res_coefficients[new_deg] = new_coeff
 		return Poly(res_coefficients)
 
-	def __mod__(self, other):
+	def __mod__(self, other: Poly):
 		other_degrees = list(other.coefficients.keys())
 		other_degrees.sort()
 
@@ -89,19 +103,32 @@ class Poly:
 			remaining -= sub
 		return remaining
 	
-	def __mul__(self, other):
+	def __mul__(self, other: Poly):
 		coeffs = dict()
 		for deg_1 in self.coefficients:
-			
 			for deg_2 in other.coefficients:
 				deg = deg_1 + deg_2
 				coeff = self.coefficients[deg_1] * other.coefficients[deg_2]
 				if deg in coeffs: # Simplify same degree members
 					coeffs[deg] += coeff
+					if coeffs[deg] == 0: # Remove the coefficient if it's useless
+						coeffs.pop(deg)
 				else:
 					coeffs[deg] = coeff
 		return Poly(coeffs)
 
+	def __eq__(self, other: Poly):
+		if not len(self.coefficients) == len(other.coefficients):
+			return False
+		
+		for k in self.coefficients:
+			if k in other.coefficients:
+				if self.coefficients[k] == other.coefficients[k]:
+					continue
+			return False
+		
+		return True
+				
 	@classmethod
 	def from_str(cls, s: str):
 		s = sub(r'\s+', '', s) # Using regex to remove all spaces
@@ -133,8 +160,21 @@ class Poly:
 				m_deg = int(m[m_deg.start():m_deg.end()]) 
 
 			coefficients[m_deg] = coeff # Populate the dict
-		return cls(coefficients)
+
+		p = cls(coefficients)
+		p.refresh()
+		return p
 	
+	# Removes coefficients equaling 0
+	def refresh(self):
+		to_remove = []
+		for d in self.coefficients:
+			if self.coefficients[d] == 0:
+				to_remove.append(d)
+
+		for i in to_remove:
+			self.coefficients.pop(i)
+
 	# Returns computed image for preimage
 	def image(self, preimage):
 		img = 0
@@ -144,12 +184,53 @@ class Poly:
 
 	# Returns the degree of the polynomial
 	def degree(self):
-		return max(list(self.coefficients.keys()))
+		return max(list(self.coefficients))
 
 	# Returns if the polynomial is a null value(a constant 0)
 	def is_zero(self):
 		return len(self.coefficients) == 0
 
 	# Returns every roots of the polynomial
-	def roots(self):
-		pass
+	def roots(self) -> ([int], [Poly]):
+		poly_dividers = []
+		remaining = Poly(self.coefficients)
+		
+		while not remaining.degree() == 1: # Divide until only a linear polynomial is left
+			# Candidates are numbers in the set dividers of x0 / dividers of self.coefficients[self.degree()]
+			candidates = []
+			if not 0 in remaining.coefficients:
+				candidates.append(0)
+			else:
+				xn = remaining.coefficients[remaining.degree()]
+				x0 = remaining.coefficients[0]
+
+				# Computing the dividers of x0 and xn
+				xn_dividers = [i for i in range(1, int(xn * 0.5) + 1) if xn % i == 0] + [xn]
+				x0_dividers = [i for i in range(1, int(x0 * 0.5) + 1) if x0 % i == 0] + [x0]
+
+				for i in x0_dividers:
+						for j in xn_dividers:
+							candidates.append(i / j)
+							candidates.append(-(i / j))
+
+			if len(candidates) == 0: # Can't factorize the polynomial anymore -> no more factors -> no more roots
+				break
+
+			# Test the candidates x so f(x) == 0
+			for c in candidates:
+				if remaining.image(c) == 0:
+					p = Poly({1: 1, 0: -c})
+					# print("f({}) = {}, dividing by {}".format(c, remaining.image(c), p))
+					remaining //= p
+					poly_dividers.append(p)
+					break
+
+		roots = []
+		for p in poly_dividers:
+			roots.append(-p.coefficients[0] / p.coefficients[1]) # 0 of linear poly(ax + b) = -b / a
+		
+		# If the remaining's degree == 1, there is a last root to compute, else it means we broke the loop because the remaining is not factorizable anymore
+		if remaining.degree() == 1:
+			roots.append(-remaining.coefficients[0] / remaining.coefficients[1]) # The root of the linear remaining polynomial
+
+		return (roots, poly_dividers + [remaining])
